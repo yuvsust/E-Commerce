cf67d089d66ccf5a51fc51454d5b04bf7b8a591a
yuv <tanvirhossainyuvraz@gmail.com>
2019-11-20 11:59:24 +0600

    T-Shirt color field is added and home page is organized more

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class product(models.Model):
 	product_image = models.ImageField(upload_to = 'product_image/')
 	catagory = models.CharField(max_length = 100)
-	title = models.CharField(max_length = 100)
+	title = models.CharField(max_length = 50)
+	color = models.CharField(max_length = 50)
 	badge = models.CharField(max_length = 100, blank=True, null=True)
 	price = models.IntegerField()
